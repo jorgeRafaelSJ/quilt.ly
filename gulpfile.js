5dd8e0039f2ab7fced731d4ee16fe5c5ea5505fb
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2016-10-04 12:00:12 -0700

    added sass and gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+
+/* == SASS == */
+gulp.task('style', function() {
+	return gulp.src('./sass/main.scss')
+		.pipe(sass.sync().on('error', sass.logError))
+		.pipe(cleanCSS({debug: true},(details) => {
+			console.log(`Original Size: ${details.stats.originalSize / 1000} KB ---> Minified Size: ${details.stats.minifiedSize / 1000} KB`);
+    }))
+		.pipe(gulp.dest('./public/styles'));
+});
+
+/* == SASS:WATCH == */
+gulp.task('style:watch', function() {
+  gulp.watch('./sass/*.scss', ['style']);
+});
