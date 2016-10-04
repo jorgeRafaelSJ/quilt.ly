27d191c65e839c844c1216783eb7f047f16748c6
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2016-10-04 14:32:49 -0700

    started aws deployment task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
+const awspublish = require('gulp-awspublish');
 
 /* == SASS == */
-gulp.task('style', function() {
+gulp.task('style', ()=> {
 	return gulp.src('./sass/main.scss')
 		.pipe(sass.sync().on('error', sass.logError))
 		.pipe(cleanCSS({debug: true},(details) => {
@@ -15,6 +16,22 @@ gulp.task('style', function() {
 });
 
 /* == SASS:WATCH == */
-gulp.task('style:watch', function() {
+gulp.task('style:watch', ()=> {
   gulp.watch('./sass/*.scss', ['style']);
+});
+
+/* == DEV DEPLOYMENT == */
+gulp.task('dev-deploy', ()=> {
+	
+	let publisher = awspublish.create({
+	  region: 'us-west-1',
+	  params: {
+	    Bucket: 'dev-web.oomamiapp.com'
+	  }
+	});
+
+	return gulp.src(['./views/index.html','./public/scripts/bundle.js','./public/styles/main.css'])
+	  .pipe(awspublish.gzip({ ext: '.gz' }))
+	  .pipe(publisher.publish()) 
+	  .pipe(awspublish.reporter());
 });
